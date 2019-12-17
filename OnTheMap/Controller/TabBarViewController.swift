56e7dcac934264aa2b98e707dbbdaf699e132544
mvirgo <mvirgs44@gmail.com>
2019-12-16 21:43:38 -0800

    feat: Add refresh and test data

---
+++ OnTheMap/Controller/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  OnTheMap
+//
+//  Created by Michael Virgo on 12/16/19.
+//  Copyright © 2019 mvirgo. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    @IBAction func refreshTapped(_ sender: Any) {
+        print("Refreshed!")
+    }
+}
