e1f36051e749c50c0e70acd7f609526c700511a2
mvirgo <mvirgs44@gmail.com>
2019-12-16 21:47:41 -0800

    refactor: move logout button to main TabBarViewController

--- OnTheMap/Controller/TabBarViewController.swift
+++ OnTheMap/Controller/TabBarViewController.swift
@@ -13,4 +13,17 @@ class TabBarViewController: UITabBarController {
     @IBAction func refreshTapped(_ sender: Any) {
         print("Refreshed!")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Change the back button to a LOGOUT button
+        self.navigationItem.hidesBackButton = true
+        let logout = UIBarButtonItem(title: "LOGOUT", style: .done, target: self, action: #selector(logoutTapped))
+        self.navigationItem.leftBarButtonItem = logout
+    }
+    
+    // TODO: Add actual logout functionality
+    // Return to the login view
+    @objc func logoutTapped(_ sender: Any) {
+        self.navigationController!.popToRootViewController(animated: true)
+    }
 }
