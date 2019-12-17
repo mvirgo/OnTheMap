11b91d4d940eab0722f6e18fa0c7a22143f7bd3b
mvirgo <mvirgs44@gmail.com>
2019-12-16 20:23:20 -0800

    feat: StudentLocation struct for incoming data

---
+++ OnTheMap/Model/StudentLocation.swift
@@ -0,0 +1,18 @@
+//
+//  StudentLocation.swift
+//  OnTheMap
+//
+//  Created by Michael Virgo on 12/16/19.
+//  Copyright © 2019 mvirgo. All rights reserved.
+//
+
+struct StudentLocation {
+    var objectId: String
+    var uniqueKey: String
+    var firstName: String
+    var lastName: String
+    var mapString: String
+    var mediaURL: String
+    var latitude: Float
+    var longitude: Float
+}
