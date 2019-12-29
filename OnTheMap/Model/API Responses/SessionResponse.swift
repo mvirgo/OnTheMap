a94cf632fe64d4cf7a8f9cfb8ee4a910c3b868e1
mvirgo <mvirgs44@gmail.com>
2019-12-28 18:08:55 -0800

    feat: response structs for login

---
+++ OnTheMap/Model/API Responses/SessionResponse.swift
@@ -0,0 +1,34 @@
+//
+//  SessionResponse.swift
+//  OnTheMap
+//
+//  Created by Michael Virgo on 12/26/19.
+//  Copyright © 2019 mvirgo. All rights reserved.
+//
+
+import Foundation
+
+struct SessionResponse: Codable {
+    let account: Account
+    let session: Session
+}
+
+struct Account: Codable {
+    let registered: Bool
+    let key: String
+    
+    enum CodingKeys: String, CodingKey {
+        case registered
+        case key
+    }
+}
+
+struct Session: Codable {
+    let id: String
+    let expiration: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case expiration
+    }
+}
