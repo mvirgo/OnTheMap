077e9ebc079656eaaebfecf49b47788cf80d6780
mvirgo <mvirgs44@gmail.com>
2020-01-01 10:35:21 -0800

    feat: basic table view for student locations

--- OnTheMap/Controller/TableViewController.swift
+++ OnTheMap/Controller/TableViewController.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UITableViewController {
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return LocationModel.locations.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
+        let location = LocationModel.locations[(indexPath as NSIndexPath).row]
+        
+        // Set the cell details
+        cell.textLabel?.text = location.firstName + " " + location.lastName
+        cell.detailTextLabel?.text = location.mediaURL
+        
+        return cell
+    }
 }
